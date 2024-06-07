bc4703219f6d7d5cd5eaee9f87b0c0c80f3fba8b
thebcrayon <106349257+thebcrayon@users.noreply.github.com>
2024-06-07 12:14:36 -0500

    Complete exercise 04 removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function (arrayToCheck, ...args) {
+    let output = arrayToCheck.filter((item) => !args.includes(item))
+    return output;
 };
 
 // Do not edit below this line
