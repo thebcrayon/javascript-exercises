b9342867c6e9036902b082e951c6b97ac8f6523f
thebcrayon <106349257+thebcrayon@users.noreply.github.com>
2024-06-07 00:16:45 -0500

    Fix exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    let output = '';
+    if(num >= 0){
+        for ( let i = 0 ; i < num ; i++){
+            output += str;
+        }
+        return output;
+    } else {
+        return 'ERROR';
+    }
+    
+   
 };
 
 // Do not edit below this line
