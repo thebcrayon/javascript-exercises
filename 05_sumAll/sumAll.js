d1df4e71e75c4faf47ff4e40ff9e22bc106e9cff
thebcrayon <106349257+thebcrayon@users.noreply.github.com>
2024-06-07 15:07:14 -0500

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function (sumStart, sumEnd) {
+    let anyTypeError = (
+        typeof sumStart !== 'number' ||
+        typeof sumEnd !== 'number' ||
+        sumStart < 0 ||
+        sumEnd < 0 
+    );
 
+    if (anyTypeError) {
+        return 'ERROR';
+    }
+
+    let startIndex = sumStart;
+    let endIndex = sumEnd;
+    let totalSum = 0;
+    
+    if (sumStart > sumEnd) {
+        startIndex = sumEnd;
+        endIndex = sumStart;
+    }
+
+    let i = startIndex;
+    while (i <= endIndex) {
+        totalSum += i;
+        i++;
+    }
+    return totalSum;
 };
 
 // Do not edit below this line
