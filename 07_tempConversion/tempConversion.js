0ac6ce290c8115b39ac2e25140dffd1aa5f2b03e
thebcrayon <106349257+thebcrayon@users.noreply.github.com>
2024-06-07 20:58:28 -0500

    Add Celsius/Fareheit conversion functions.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (value) {
+  return parseFloat(((value - 32) * (5 / 9)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (value) {
+  return parseFloat((((9 / 5) * value) + 32).toFixed(1));
 };
 
 // Do not edit below this line
