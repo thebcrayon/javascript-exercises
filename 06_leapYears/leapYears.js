81b78d29b234aae7a7c82047362fcc473506f2cf
thebcrayon <106349257+thebcrayon@users.noreply.github.com>
2024-06-07 19:12:32 -0500

    Add logic for leap year exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function (year) {
+    let yearNormal = year % 4 == 0;
+    let yearCenturyD100 = year % 100 == 0;
+    let yearCenturyD400 = year % 400 == 0;
 
+    if (yearNormal && yearCenturyD100) {
+        if (yearCenturyD400) {
+            return true;
+        } else {
+            return false;
+        }
+    } else if (yearNormal) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
