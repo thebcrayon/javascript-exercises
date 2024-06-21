17aebb816836b7f83fdad3be29b714c710434638
thebcrayon <106349257+thebcrayon@users.noreply.github.com>
2024-06-21 13:30:11 -0500

    Code refactor, comment out my original solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,18 +1,30 @@
 const palindromes = function (string) {
-    let regEx = /[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g;
-    const orginalStringArray = string.toLowerCase().replace(regEx, '').split('');
-    const reversedStringArray = string.toLowerCase().replace(regEx, '').split('').reverse();
-    let isPalindrome = true;
+    const allowedCharacters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
-    for (let i = 0; i < orginalStringArray.length; i++) {
-        let letterMatch = (orginalStringArray[i] === reversedStringArray[i]);
-        if(!letterMatch){
-            isPalindrome = false;
-            break;
-        }
-    }
+    const regularString = string
+        .toLowerCase()
+        .split('')
+        .filter(character => allowedCharacters.includes(character))
+        .join('');
 
-    return isPalindrome;
+    const reversedString = regularString.split('').reverse().join('');
+
+    return (regularString === reversedString);
+
+    // let regEx = /[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g;
+    // const orginalStringArray = string.toLowerCase().replace(regEx, '').split('');
+    // const reversedStringArray = string.toLowerCase().replace(regEx, '').split('').reverse();
+    // let isPalindrome = true;
+
+    // for (let i = 0; i < orginalStringArray.length; i++) {
+    //     let letterMatch = (orginalStringArray[i] === reversedStringArray[i]);
+    //     if(!letterMatch){
+    //         isPalindrome = false;
+    //         break;
+    //     }
+    // }
+
+    // return isPalindrome;
 };
 
 // Do not edit below this line
