23e93957ddf441b0a8c006970a6f43340cf290d7
thebcrayon <106349257+thebcrayon@users.noreply.github.com>
2024-06-21 13:06:34 -0500

    Complete palindromes exercise! :D

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let regEx = /[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g;
+    const orginalStringArray = string.toLowerCase().replace(regEx, '').split('');
+    const reversedStringArray = string.toLowerCase().replace(regEx, '').split('').reverse();
+    let isPalindrome = true;
 
+    for (let i = 0; i < orginalStringArray.length; i++) {
+        let letterMatch = (orginalStringArray[i] === reversedStringArray[i]);
+        if(!letterMatch){
+            isPalindrome = false;
+            break;
+        }
+    }
+
+    return isPalindrome;
 };
 
 // Do not edit below this line
