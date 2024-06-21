79734808abe46d84329395a7c40160c1e19d5999
thebcrayon <106349257+thebcrayon@users.noreply.github.com>
2024-06-21 17:45:26 -0500

    Complete 11 getTheTitles with reduce

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    return array.reduce((ary, book, index) => {
+        ary[index] = book.title; 
+        return ary;
+    }, []);
 };
 
 // Do not edit below this line
