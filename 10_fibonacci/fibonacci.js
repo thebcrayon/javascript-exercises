4ecec3238a37f0f62fa3c2cadb744348af972487
thebcrayon <106349257+thebcrayon@users.noreply.github.com>
2024-06-21 17:23:24 -0500

    Show my solution for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function (pos) {
+    const seq = [1,1,2,3];
+    let num = 0;
 
+    if (pos > 0){
+        for (let i = 0 ; i < pos ; i++){
+            num = seq[seq.length - 1] + seq[seq.length - 2];
+            seq.push(num);
+        }
+    } else if (pos < 0){
+        return 'OOPS';
+    } else {
+        return 0;
+    }
+
+    //starting from i = 1
+    //at each iteration of i, calculate current value based on 2 numbers before
+    //push that total to the next avaialble space in sequence.
+    //take the new sequence and build upon it with num = i - 2 + i - 1 
+    //push that value to end of sequence array.
+    //do this up to nth argument.
+    return seq[pos-1];
 };
 
 // Do not edit below this line
