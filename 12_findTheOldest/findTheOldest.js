8fd6b324e4f203ed7c6fb35190d3b806d18a8740
thebcrayon <106349257+thebcrayon@users.noreply.github.com>
2024-06-22 00:05:32 -0500

    Complete exercise 12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (peopleArray) {
+    let sorted = peopleArray.sort((a, b) => {
+        let currentDate;
 
+        if (a.yearOfDeath === undefined || b.yearOfDeath === undefined) {
+            currentDate = (new Date()).getFullYear();
+        }
+
+        let ageA = (a.yearOfDeath || currentDate) - a.yearOfBirth;
+        let ageB = (b.yearOfDeath || currentDate) - b.yearOfBirth;
+
+        if (ageA > ageB ){
+            return -1;
+        } else {
+            return 1;
+        }
+    });
+
+    return sorted[0];
 };
 
 // Do not edit below this line
